dcd5fdd8bbc914b060006d21840d889914ee3156
Humza23 <79542316+Humza23@users.noreply.github.com>
2022-03-03 14:18:57 -0500

    works without form

--- fetchData.js
+++ fetchData.js
@@ -1,7 +1,23 @@
 const API_KEY = '00b908352099ba90a12ecbd4a449112b'
 
-fetch("https://api.openweathermap.org/data/2.5/weather?q=brooklyn&appid=00b908352099ba90a12ecbd4a449112b")
+fetch("https://api.openweathermap.org/data/2.5/weather?q=brooklyn&units=imperial&appid=00b908352099ba90a12ecbd4a449112b")
 .then((data) => data.json())
+
 .then((jsonData) => {
-    console.log(jsonData);
+
+    // console.log(jsonData);
+    fetch(`http://openweathermap.org/img/w/${jsonData.weather[0].icon}.png`)
+    .then((res) => res.blob())
+    .then((result) => {
+        document.getElementById("text_location").innerHTML = jsonData.name
+        document.getElementById("text_location_country").innerHTML = jsonData.sys.country
+    
+        document.getElementById("text_temp").innerHTML = Math.round(jsonData.main.temp)
+        document.getElementById("text_feelslike").innerHTML = Math.round(jsonData.main.feels_like)
+        document.getElementById("text_desc").innerHTML = jsonData.weather[0].description
+
+        const imageObjectURL = URL.createObjectURL(result)
+        document.getElementById("icon").src = imageObjectURL
+    })
+
 })
